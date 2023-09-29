56b2ce5aba24434b0a425f706661cad3b60c44a9
Cuates <48161526+Cuates@users.noreply.github.com>
2023-09-29 14:14:11 -0700

    Update extractMediaFeed.sql

--- mssql/Stored_Procedure/extractMediaFeed.sql
+++ mssql/Stored_Procedure/extractMediaFeed.sql
@@ -17,7 +17,7 @@ go
 -- =================================================
 --               File: extractMediaFeed
 --        Created: 10/28/2020
---        Updated: 09/15/2023
+--        Updated: 09/29/2023
 --  Programmer: Cuates
 --    Update By: Cuates
 --        Purpose: Extract Media Feed
@@ -66,8 +66,8 @@ begin
 
   -- Set variables
   set @omitOptionMode = N'0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-  set @omitTitleLong = N'-,.,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
-  set @omitTitleShort = N'-,.,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+  set @omitTitleLong = N'-,.,'',0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+  set @omitTitleShort = N'-,.,'',0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
   set @omitInfoUrl = N'-,.,/,%,?,=,&,:,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
   set @omitActionStatus = N'0,1,2,3,4,5,6,7,8,9'
   set @omitLimit = N'-,0,1,2,3,4,5,6,7,8,9'
